7dbc28dfcc8e5c81e154a83c732450648b1af447
pnhan <pnhan@ualberta.ca>
2014-09-30 17:14:50 -0600

    4th commit

--- statsandprint.py
+++ statsandprint.py
@@ -4,17 +4,38 @@ import start
 import blockreciever
 import control
 
+def standard_dev(lists):
+    avg = float(sum(lists))/len(lists)
+    #print(avg)
+    dev = []
+    for x in lists:
+        dev.append(x - avg)
+        #print(dev)
+    sqr = []
+    for x in dev:
+        sqr.append(x * x)
+    #print(sqr)
+    mean = sum(sqr)/len(sqr)
+    #print(mean)
+    standard_deviation = math.sqrt(sum(sqr)/(len(sqr)-1))    
+    return standard_deviation
 
 def getandprintstats(arrayofframes, arrayofthoroughput, arrayofseeds, arrayofinput ):
 
     T = arrayofinput[5]
-    #Framestddev = statistics.stdev(arrayofframes)
-    Framestddev = numpy.std(arrayofframes)
-    #Thoroughputstddev = statistics.stdev(arrayofthoroughput)
+    Framestddev = standard_dev(arrayofframes)
+    #Framestddev = numpy.std(arrayofframes)
+    Thoroughputstddev = standard_dev(arrayofthoroughput)
     #Thoroughputstddev = numpy.stdev(arrayofthoroughput)   
     
-    standard_dev= math.sqrt(average(
+    #standard deviation
+    
+    
+    #standard deviation end
+    
+    
     Framesum=0
+    
     
     for numofframes in range(arrayofframes):
         Framesum = Framesum+numofframes
